b69caa7aa59c76baf0a94f022b0f9f41e51abf56
Ankita <ankyadav1999@gmail.com>
2021-03-30 20:42:48 +0530

    first cut

---
+++ Socialinfo - Copy.py
@@ -0,0 +1,30 @@
+while True:
+    print("1.Bhagat Singh ")
+    print("2.Dr.Babasaheb Ambedkar")
+    print("3.Savitribai Phule")
+    print("4.Shivaji Maharaj")
+    print("5.Exit")
+   
+
+    choice = int(input("Enter Your Choice: "))
+
+    if (choice == 1):
+        print("\n Your Choice is Bhagat Singh, here is what wikipidia says\n")
+        print("\n Bhagat Singh (Punjabi pronunciation: [pə̀ɡət̪ sɪ́ŋɡ] (About this soundlisten) 1907[a] – 23 March 1931) was an Indian socialist revolutionary whose two acts of dramatic violence against the British in India and execution at age 23 made him a folk hero of the Indian independence movement.\n\n")
+        print("------------------------")
+    elif(choice == 2):
+        print("\n Your Choice is Dr.Babsaheb Ambedkar, here is what wikipidia says\n ")
+        print("\n Bhimrao Ramji Ambedkar, also known as Babasaheb Ambedkar, was an Indian jurist, economist, politician and social reformer, who inspired the Dalit Buddhist movement and campaigned against social discrimination towards the untouchables. He was a member of the Constituent Drafting committee. \n\n")
+        print("------------------------")
+    elif(choice == 3):
+        print("\n Your Choice is Savitribai Phule, here is what wikipidia says\n")
+        print("\n Savitribai Phule was an Indian social reformer, educationalist, and poet from Maharashtra. She is regarded as the first female teacher of India. Along with her husband, Jyotirao Phule, she played an important and vital role in improving women's rights in India. She is regarded as the mother of Indian. \n\n")
+        print("------------------------")
+    elif(choice == 4):
+        print("\n Your Choice is Shivaji Maharaj, here  is what wikipidia says\n")
+        print("\n Shivaji Bhonsale I was an Indian ruler and a member of the Bhonsle Maratha clan. Shivaji carved out an enclave from the declining Adilshahi sultanate of Bijapur that formed the genesis of the Maratha Empire. In 1674, he was formally crowned as the Chhatrapati of his realm at Raigad. \n\n")
+        print("------------------------")
+    else:
+        print("\n\n Sorry, Your Choice Is Wrong")
+    
+    
